a941e8502a52a8fedde8c2ca087b03ae20f59769
jrbonnke <95120254+jrbonnke@users.noreply.github.com>
2025-03-06 18:07:08 +0530

    Create terraform-sample.tf

---
+++ terraform-sample.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "my-terraform-state"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-12345678"
+  instance_type = var.instance_type
+  tags = {
+    Name = "Terraform-Instance"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install -y httpd",
+      "sudo systemctl start httpd"
+    ]
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
